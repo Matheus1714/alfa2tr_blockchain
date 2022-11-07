61204b98d153d5553ed00d2f24b5c662af32a5da
matheus1714 <matheussilvamartins1714@gmail.com>
2022-11-07 01:16:03 -0300

    create route get_chain and mine_block

--- routes.py
+++ routes.py
@@ -1,8 +1,32 @@
 from flask import jsonify, request
-from src.models.Blockchain import BlockChain
+from src.models.Blockchain import Blockchain
 import requests
 from uuid import uuid4
 from urllib.parse import urlparse
 
-def routes(app, blockchain):
-    pass
+def routes(app, blockchain:Blockchain):
+    
+    @app.route('/mine_block', methods = ['GET'])
+    def mine_block():
+        previous_block = blockchain.get_previous_block()
+        previous_proof = previous_block['proof']
+        proof = blockchain.proof_of_work(previous_proof)
+        previous_hash = blockchain.hash(previous_block)
+        block = blockchain.create_block(proof, previous_hash)
+        response = {
+            'msg':'mine block',
+            'index': block['index'],
+            'timestamp': block['timestamp'],
+            'proof': block['proof'],
+            'previous_hash': block['previous_hash']
+        }
+        return jsonify(response), 200
+
+    @app.route('/get_chain', methods = ['GET'])
+    def get_chain():
+        response = {
+            'chain': blockchain.chain,
+            'length': len(blockchain.chain)
+        }
+        return jsonify(response), 200
+
