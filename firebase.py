8eaad5030d673afb09ef38febc54e4dc13cdee65
matheus1714 <matheussilvamartins1714@gmail.com>
2022-11-07 01:15:44 -0300

    create basic example firebase

--- firebase.py
+++ firebase.py
@@ -1,4 +1,41 @@
 from src.db.Firestore import Firestore
+from src.utils.CryptoUtils import CryptoUtils
 
-# firestore = Firestore()
+firestore = Firestore()
+crypto = CryptoUtils()
 
+from datetime import datetime
+
+blockchain = [
+    {
+        'id':crypto.generate_key(),
+        'timestamp': datetime.now(),
+        'nonce': 123123,
+        'hash': crypto.generate_hash('1'),
+        'previous_hash': crypto.generate_hash('0')
+    },
+    {
+        'id':crypto.generate_key(),
+        'timestamp': datetime.now(),
+        'nonce': 123123,
+        'hash': crypto.generate_hash('2'),
+        'previous_hash': crypto.generate_hash('1')
+    },
+    {
+        'id':crypto.generate_key(),
+        'timestamp': datetime.now(),
+        'nonce': 123123,
+        'hash': crypto.generate_hash('3'),
+        'previous_hash': crypto.generate_hash('2')
+    },
+    {
+        'id':crypto.generate_key(),
+        'timestamp': datetime.now(),
+        'nonce': 123123,
+        'hash': crypto.generate_hash('4'),
+        'previous_hash': crypto.generate_hash('3')
+    },
+]
+
+for block in blockchain:
+    firestore.add_json_obj(block, collection_name='blockchain', document_name=crypto.generate_key())
