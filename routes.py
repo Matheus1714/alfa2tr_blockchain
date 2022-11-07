fcdb20239a56c44a4b958e7e95cabd13105bf661
matheus1714 <matheussilvamartins1714@gmail.com>
2022-11-07 12:51:24 -0300

    config routes to add blockchain

--- routes.py
+++ routes.py
@@ -12,7 +12,7 @@ def routes(app, blockchain:Blockchain):
         previous_proof = previous_block['proof']
         proof = blockchain.proof_of_work(previous_proof)
         previous_hash = blockchain.hash(previous_block)
-        block = blockchain.create_block(proof, previous_hash)
+        block, id = blockchain.create_block(proof, previous_hash)
         response = {
             'msg':'mine block',
             'index': block['index'],
