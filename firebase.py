e2575cffe12eb0ea5ec0308839dce8eba93e9d68
matheus1714 <matheussilvamartins1714@gmail.com>
2022-11-06 22:40:49 -0300

    config firebase

---
+++ firebase.py
@@ -0,0 +1,11 @@
+import pyrebase
+
+firebase_config = {
+    'apiKey': "AIzaSyAqEHlmjLNXfhvkv9HZExoApw4C3LyLkZU",
+    'authDomain': "alfa2tr.firebaseapp.com",
+    'projectId': "alfa2tr",
+    'storageBucket': "alfa2tr.appspot.com",
+    'messagingSenderId': "833286290040",
+    'appId': "1:833286290040:web:cb172431181216ecb44b8d",
+    'measurementId': "G-8JJ9J6B22H"
+}
