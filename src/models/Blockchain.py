95951ebe8dc8bdaa809e2ec401193e97f3b91526
matheus1714 <matheussilvamartins1714@gmail.com>
2022-11-07 01:16:39 -0300

    create generic blockchain

---
+++ src/models/Blockchain.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+import json
+
+from src.utils.CryptoUtils import CryptoUtils
+from src.db.Firestore import Firestore
+
+class Blockchain:
+
+    cryoto = CryptoUtils()
+
+    def __init__(self) -> None:
+        self.chain = []
+        self.create_block(proof=1, previous_hash='0')
+    
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.now()),
+            'proof': proof,
+            'previous_hash': previous_hash
+        }
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
+    
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            dproof = new_proof**2 - previous_proof**2
+            hash_operation = self.cryoto.generate_hash(str(dproof))
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True)
+        return self.cryoto.generate_hash(encoded_block)
+    
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            dproof = proof**2 - previous_proof**2
+            hash_operation = self.cryoto.generate_hash(str(dproof))
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = block
+            block_index += 1
+        return True
+
+    
