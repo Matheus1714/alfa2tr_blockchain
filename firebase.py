8be3218c4d37a78d448b412f18e35a673f877930
matheus1714 <matheussilvamartins1714@gmail.com>
2022-11-07 00:02:14 -0300

    file test firebase

--- firebase.py
+++ firebase.py
@@ -1,12 +1,4 @@
-import pyrebase
+from src.db.Firestore import Firestore
 
-firebase_config = {
-    'apiKey': "AIzaSyAqEHlmjLNXfhvkv9HZExoApw4C3LyLkZU",
-    'authDomain': "alfa2tr.firebaseapp.com",
-    'projectId': "alfa2tr",
-    'storageBucket': "alfa2tr.appspot.com",
-    'messagingSenderId': "833286290040",
-    'appId': "1:833286290040:web:cb172431181216ecb44b8d",
-    'measurementId': "G-8JJ9J6B22H"
-}
+# firestore = Firestore()
 
