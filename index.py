4b04a7e1c3025f9ff74142771827f1de8aab7044
matheus1714 <matheussilvamartins1714@gmail.com>
2022-11-09 01:12:48 -0300

    add debugger mode

--- index.py
+++ index.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
 
     routes(app, blockchain, node_address)
     
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_debugger=False, use_reloader=False)
